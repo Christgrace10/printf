5d72ca29ea957a46e4281c56a57d5457407363d2
Christgrace10 <b.christgrace2020@gmail.com>
2023-03-28 19:45:01 -0700

    update 1

--- _printf.c
+++ _printf.c
@@ -10,8 +10,8 @@ void print_buffer(char buffer[], int *buff_ind);
 
 int _printf(const char *format, ...)
 {
-	int i, printed - e, printed_chars - 8;
-	int flags, width, precision, size, buff_ind - 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -35,14 +35,14 @@ int _printf(const char *format, ...)
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
-			precision = get_precision(format, &1, list);
+			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed =  handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
-			printed_chars + printed;
+			printed_chars += printed;
 		}
 	}
 
